79ef4cc68ed573864cc15b14000c5972d45ed586
alisebeth <elizaveta.05031996@yandex.ru>
2023-01-26 15:21:11 +0300

    update: done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function reverse (n) {
-  
+  let newNum = '';
+  let num = n;
+  if (n < 0) {
+    num = Math.abs(num);
+  }
+  let number = String(num);
+  for (let i = 0; i < number.length; i++) {
+    newNum = `${number[i]}${newNum}`;
+  }
+  newNum = Number(newNum);
+  return newNum;
 }
